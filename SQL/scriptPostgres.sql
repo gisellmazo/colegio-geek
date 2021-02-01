9ace4207a785253e443bc7bc23baf75715f77c59
Diego Buitrago <die.ar.90@gmail.com>
2021-02-01 15:45:22 -0500

    cambios

--- SQL/scriptPostgres.sql
+++ SQL/scriptPostgres.sql
@@ -1,5 +1,19 @@
 CREATE DATABASE colegio_geek;
 
+--- Eliminar relaciones
+ALTER TABLE materias DROP CONSTRAINT fk_materias_profesores ;
+ALTER TABLE materias DROP CONSTRAINT fk_materias_grados;
+ALTER TABLE estudiantes DROP CONSTRAINT fK_estudiantes_grupos;
+ALTER TABLE profesores DROP CONSTRAINT fk_profesores_materias;
+ALTER TABLE grados_cursados DROP CONSTRAINT fk_grados_cursados_estudiantes;
+ALTER TABLE grupos DROP CONSTRAINT fk_grupos_profesores;
+ALTER TABLE grupos DROP CONSTRAINT fk_grupos_grados;
+ALTER TABLE grados DROP CONSTRAINT fk_grados_materias;
+ALTER TABLE notas DROP CONSTRAINT fk_notas_estudiante;
+ALTER TABLE notas DROP CONSTRAINT fk_notas_grupos;
+ALTER TABLE notas DROP CONSTRAINT fk_notas_materias;
+
+-- Eliminar tablas
 DROP TABLE IF EXISTS administrador
 DROP TABLE IF EXISTS materias;
 DROP TABLE IF EXISTS estudiantes;
@@ -9,6 +23,17 @@ DROP TABLE IF EXISTS grupos;
 DROP TABLE IF EXISTS grados;
 DROP TABLE IF EXISTS notas;
 
+-- Crear campos enum
+CREATE TYPE documento AS ENUM('TI''CC','NUIP');
+CREATE TYPE sexo AS ENUM('F','M');
+CREATE TYPE tipo_documento AS ENUM('CC','CE');
+CREATE TYPE grado_cursado AS ENUM('6','7', '8', '9', '10', '11');
+CREATE TYPE estado AS ENUM('1','2', '3');
+CREATE TYPE jornada AS ENUM('1','2', '3');
+CREATE TYPE grado AS ENUM('6','7', '8', '9', '10', '11');
+CREATE TYPE tipo_nota AS ENUM('nota 1','nota 2', 'nota 3', 'parsial', 'fina');
+
+-- Crear tablas
 CREATE TABLE administrador(
   id_admin SERIAL PRIMARY KEY,
   numero_documento VARCHAR(50) UNIQUE NOT NULL,
@@ -24,8 +49,6 @@ CREATE TABLE materias(
     id_grados INTEGER NOT NULL
 );
 
-CREATE TYPE documento AS ENUM('TI''CC','NUIP');
-CREATE TYPE sexo AS ENUM('F','M');
 CREATE TABLE estudiantes(
     id_estudiante SERIAL PRIMARY KEY,
     id_grupo INTEGER UNIQUE NOT NULL,
@@ -44,7 +67,6 @@ CREATE TABLE estudiantes(
     tipo_usuario INTEGER DEFAULT 3 NOT NULL
 );
 
-CREATE TYPE tipo_documento AS ENUM('CC','CE');
 CREATE TABLE profesores(
     id_profesor SERIAL PRIMARY KEY,
     id_materia INTEGER UNIQUE NOT NULL,
@@ -56,19 +78,15 @@ CREATE TABLE profesores(
     tipo_usuario INTEGER DEFAULT 2 NOT NULL
 );
 
-CREATE TYPE grado AS ENUM('6','7', '8', '9', '10', '11');
-CREATE TYPE estado AS ENUM('1','2', '3');
 CREATE TABLE grados_cursados(
     id_grado_cursado SERIAL PRIMARY KEY,
     id_estudiante integer UNIQUE NOT NULL,
     ano VARCHAR(10) NOT NULL,
-    grado grado NOT NULL,
+    grado_cursado grado_cursado NOT NULL,
     estado estado NOT NULL,
     nota_promedio INTEGER
 );
 
-
-CREATE TYPE jornada AS ENUM('1','2', '3');
 CREATE TABLE grupos(
     id_grupo SERIAL PRIMARY key,
     codigo_grupo VARCHAR(10) UNIQUE NOT NULL,
@@ -77,14 +95,12 @@ CREATE TABLE grupos(
     jornada jornada NOT NULL
 );
 
-CREATE TYPE grado AS ENUM('6','7', '8', '9', '10', '11');
 CREATE TABLE grados(
     id_grado SERIAL PRIMARY KEY,
     grado grado NOT NULL,
     id_materias INTEGER NOT NULL
 ); 
 
-CREATE TYPE tipo_nota AS ENUM('nota 1','nota 2', 'nota 3', 'parsial', 'fina');
 CREATE TABLE notas(
     id_notas SERIAL PRIMARY KEY,
     id_estudiante INTEGER UNIQUE NOT NULL,
@@ -94,41 +110,42 @@ CREATE TABLE notas(
     nota FLOAT NOT NULL
 );
 
+-- Crear relaciones
 ALTER TABLE materias
-  ADD CONSTRAINT materias_profesores 
-  FOREIGN KEY(id_profesor) REFERENCES profesores(id_profesor);
-  ADD CONSTRAINT materias_grados 
+  ADD CONSTRAINT fk_materias_profesores 
+  FOREIGN KEY(id_profesor) REFERENCES profesores(id_profesor),
+  ADD CONSTRAINT fk_materias_grados 
   FOREIGN KEY(id_grados) REFERENCES grados(id_grado);
 
 ALTER TABLE estudiantes
-  ADD CONSTRAINT estudiantes_grupos 
+  ADD CONSTRAINT fK_estudiantes_grupos 
   FOREIGN KEY(id_grupo)
   REFERENCES grupos(id_grupo);
 
 ALTER TABLE profesores
-  ADD CONSTRAINT profesores_materias 
+  ADD CONSTRAINT fk_profesores_materias 
   FOREIGN KEY(id_materia)
   REFERENCES materias(id_materia);
 
 ALTER TABLE grados_cursados
-  ADD CONSTRAINT grados_cursados_estudiantes 
+  ADD CONSTRAINT fk_grados_cursados_estudiantes 
   FOREIGN KEY(id_estudiante)
   REFERENCES estudiantes(id_estudiante);
 
 ALTER TABLE grupos
-  ADD CONSTRAINT grupos_profesores 
-  FOREIGN KEY(id_profesor) REFERENCES profesores(id_profesor);
-  ADD CONSTRAINT grupos_grados 
-  FOREIGN KEY(id_grado) REFERENCES grado(id_grado);
+  ADD CONSTRAINT fk_grupos_profesores 
+  FOREIGN KEY(id_profesor) REFERENCES profesores(id_profesor),
+  ADD CONSTRAINT fk_grupos_grados 
+  FOREIGN KEY(id_grado) REFERENCES grados(id_grado);
 
 ALTER TABLE grados
-  ADD CONSTRAINT grados_materias 
+  ADD CONSTRAINT fk_grados_materias 
   FOREIGN KEY(id_materias) REFERENCES materias(id_materia);
 
 ALTER TABLE notas
-  ADD CONSTRAINT notas_estudiante 
+  ADD CONSTRAINT fk_notas_estudiante 
   FOREIGN KEY(id_estudiante) REFERENCES estudiantes(id_estudiante),
-  ADD CONSTRAINT notas_grupos 
+  ADD CONSTRAINT fk_notas_grupos 
   FOREIGN KEY(id_grupo) REFERENCES grupos(id_grupo),
-  ADD CONSTRAINT notas_materias
+  ADD CONSTRAINT fk_notas_materias
   FOREIGN KEY(id_materia) REFERENCES materias(id_materia);
