895f9a6ef24e7d2d6b25f27b98707c2af79506d9
JuanEstebanCC <juanescifuentes75@gmail.com>
2021-02-02 18:11:40 -0500

    Almost done all services

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.set('port', process.env.PORT || 5600);
+app.set('port', process.env.PORT || 5700);
 
 app.use(require('./send_mail/send_mail'));
 
--- routes/profesor/profesor.js
+++ routes/profesor/profesor.js
@@ -3,7 +3,6 @@ const router = Router();
 
 const { pool } = require('../../database/database');
 
-
 // Ver Perfil Profesor
 router.get('/perfil_profesor', async (req, res) => {
   try {
@@ -20,16 +19,26 @@ router.get('/perfil_profesor', async (req, res) => {
 
 module.exports = router;
 
-// Ver Estudiantes Profesor 
-router.get('/ver_estudiantes_profesor', async (req,res) =>{
-  try{
-    const { id_profesor } = req.query;
+// Ver Estudiantes Profesor
+router.get('/ver_estudiantes_profesor', async (req, res) => {
+  try {
+    const { id_grado } = req.query;
     const response = await pool.query(
-      `SELECT nombres_apellidos,numero_documento,correo FROM profesores WHERE id_profesor = $1`,
-      [id_profesor]
+      `SELECT nombres_apellidos,id_grupo FROM estudiantes `
     );
     return res.json(response.rows);
-  }catch(error){
+  } catch (error) {
     console.log(error);
   }
-})
+});
+
+// Ver Grupos Profesor
+router.get('/ver_grupos_profesor', async (req, res) => {
+  try {
+   const response = await pool.query(`SELECT codigo_grupo,jornada FROM grupos`)
+   return res.json(response.rows)
+   
+  } catch (error) {
+    console.log(error);
+  }
+});
