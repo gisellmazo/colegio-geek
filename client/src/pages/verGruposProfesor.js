7539b38c628674ca9eba32f1f5b192783477c5df
Gisell Mazo <gisellmazo@gmail.com>
2021-02-04 23:05:57 -0500

    traer de bd datos en servicios de profesor

--- client/src/pages/verGruposProfesor.js
+++ client/src/pages/verGruposProfesor.js
@@ -69,7 +69,7 @@ function VerGruposProfesor() {
                         Grupo: {datos[0].id_grupo} <br />
                         Código del grupo: {datos[0].codigo_grupo} <br />
                         Materia: {datos[0].id_materia}  <br />
-                        Número de estudiantes:
+                        Número de estudiantes:{datos[0].id_estudiante} 
                       </p>
                     </div>
                   </div>
