ce014ee3487a2adeaa924f329a5cb4f7caadfae1
JuanEstebanCC <juanescifuentes75@gmail.com>
2021-02-06 19:40:38 -0500

    All services for the reports done

--- server/routes/administrador/administrador.js
+++ server/routes/administrador/administrador.js
@@ -477,60 +477,7 @@ router.get('/reporte_calificaciones_por_estudiante', async (req, res) => {
   );
 });
 
-router.get('/promedio_notas_grupo', async (req, res) => {
-  const client = await pool.connect();
-  client.query(
-    `SELECT avg(nota) as promedio_notas, grupos.id_grupo
-  FROM notas 
-  INNER JOIN grupos ON notas.id_grupo = grupos.id_grupo
-  group by grupos.id_grupo`,
-    (error, resulset) => {
-      client.release(true);
-      if (error) {
-        console.log(error);
-        return res
-          .status(500)
-          .send('Se presento un error en la base de datos.');
-      } else {
-        return res.json(resulset.rows);
-      }
-    }
-  );
-});
 
-//servicio promedio de notas por materia
-router.get('/promedio_notas_materia', async (req, res) => {
-  const client = await pool.connect();
-  client.query(`SELECT avg(nota) as promedio_notas, materias.id_materia
-  FROM notas 
-  INNER JOIN materias ON notas.id_materia = materias.id_materia
-  group by materias.id_materia`, (error, resulset) => {
-    client.release(true);
-    if (error) {
-      console.log(error)
-      return res.status(500).send('Se presento un error en la base de datos.');
-    } else {
-      return res.json(resulset.rows);
-    }
-  });
-});
-// Promedio notas por grado
-router.get('/promedio_notas_grado', async (req, res) => {
-  const client = await pool.connect();
-  client.query(`SELECT avg(nota) as promedio_notas, grados.id_grado
-  FROM notas 
-  INNER JOIN grupos ON grupos.id_grado = notas.id_grupo 
-  INNER JOIN grados ON grados.id_grado = notas.id_grupo
-  group by grados.id_grado`, (error, resulset) => {
-    client.release(true);
-    if (error) {
-      console.log(error)
-      return res.status(500).send('Se presento un error en la base de datos.');
-    } else {
-      return res.json(resulset.rows);
-    }
-  });
-});
 
 
 module.exports = router;
